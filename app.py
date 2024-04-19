3908a558af5a5e3c51607b5c705ddc3a2f40e1c9
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-19 16:06:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+st.title("This is my hello world app")
+st.header("This is a header")
+st.subheader("This is a subheader")
 st.write("hello world")
 
 #This markdown for showing html website
@@ -13,8 +15,14 @@ In this form will collect info following as below
 
 _We do not sell these data_
 """
-
 name = st.text_input("Enter your name")
 if name:
     st.write(f"your name is {name}")
 
+age = st.slider("Enter your age")
+if age:
+    st.write(f"your name is {age}")
+
+location = st.text_input("Enter your location")
+if location:
+    st.write(f"your name is {location}")
