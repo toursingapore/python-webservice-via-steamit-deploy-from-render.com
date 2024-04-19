3b4a9c8849dffff6f3d73253e70ebb7152e4b34a
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-19 15:48:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
 st.write("hello world")
+
+#Full html by markdown
+"""
+# This is my login form
+
+In this form will collect info following as below
+* Name
+* Age
+* Location
+
+_We do not sell these data_
+"""
